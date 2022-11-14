f3f03e26019805b5b7bd9a2b6bb81a0ba16101a0
Tran Van Nam <namtran10a2nt@gmail.com>
2022-11-14 15:12:28 +0700

    update userManage

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,20 +1,76 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import { getAllUsers } from '../../services/userService';
+import './UserManage.scss';
 class UserManage extends Component {
 
-    state = {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            arrUsers: []
+        }
     }
 
-    componentDidMount() {
-
+    async componentDidMount() {
+        let response = await getAllUsers('All');
+        console.log(response);
+        if (response && response.data.errCode === 0) {
+            this.setState({
+                arrUsers: response.data.users
+            })
+        }
     }
+
+    /**
+     * Lifecycle
+     * Run component:
+     * Đầu tiên chạy contructor -> init state
+     * Sau khi render lần đầu xong thì hàm componentDidMount() sẽ chạy
+     * khi state cập nhật thì component sẽ tự động render lại
+     */
 
 
     render() {
+        let arrUsers = this.state.arrUsers;
         return (
-            <div className="text-center">Manage users</div>
+            <div className="users-container">
+                <div className='title text-center mb-3'>MANAGE USERS WITH NAM</div>
+                <div className='users-table mx-2'>
+                    <table id="customers">
+                        <tr>
+                            <th>Email</th>
+                            <th>First name</th>
+                            <th>Last name</th>
+                            <th>Address</th>
+                            <th>Actions</th>
+                        </tr>
+
+                        {arrUsers && arrUsers.map((item, index) => {
+                            return (
+                                <tr key={index}>
+                                    <td>{item.email}</td>
+                                    <td>{item.firstName}</td>
+                                    <td>{item.lastName}</td>
+                                    <td>{item.address}</td>
+                                    <td>
+                                        <button className='btn-edit'>
+                                            <i class="fas fa-pencil-alt"></i>
+                                        </button>
+                                        <button className='btn-delete'>
+                                            <i class="fas fa-trash"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                            )
+                        })
+                        }
+
+
+                    </table>
+
+                </div>
+            </div>
         );
     }
 
